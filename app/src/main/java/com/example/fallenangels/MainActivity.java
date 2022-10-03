944fb3b2504ea1732c300ab37d50fe98ae8a7628
BenjiFrye <benjaminsfrye@gmail.com>
2022-10-03 17:05:13 +0200

    Start

--- app/src/main/java/com/example/fallenangels/MainActivity.java
+++ app/src/main/java/com/example/fallenangels/MainActivity.java
@@ -6,7 +6,7 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
-    private String hello;
+    private String hello = "hello world";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
