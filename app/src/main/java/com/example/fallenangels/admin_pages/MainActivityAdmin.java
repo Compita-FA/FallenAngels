42aeb61b4f2296a1abb7a78fc3703337c1d0faaf
BenjiFrye <benjaminsfrye@gmail.com>
2022-10-05 15:10:24 +0200

    Admin pages

--- app/src/main/java/com/example/fallenangels/admin_pages/MainActivityAdmin.java
+++ app/src/main/java/com/example/fallenangels/admin_pages/MainActivityAdmin.java
@@ -6,11 +6,11 @@ import android.os.Bundle;
 
 import com.example.fallenangels.R;
 
-public class AdminDashboard extends AppCompatActivity {
+public class MainActivityAdmin extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_admin_dashboard);
+        setContentView(R.layout.activity_main_admin);
     }
 }
